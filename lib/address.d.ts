35a09ed482d7c7d21686cfbab331c7b9b0e215a9
Ratson <github@ratson.name>
2019-05-18 14:19:54 +0800

    Fix TypeScript declaration

--- lib/address.d.ts
+++ lib/address.d.ts
@@ -1,4 +1,4 @@
-namespace address {
+declare namespace address {
   function ip(interfaceName?: string): string
 
   function ipv6(interfaceName?: string): string 
@@ -10,7 +10,7 @@ namespace address {
   function dns(filePath: string, callback: (err: Error, servers: string[]) => void): void
 }
 
-function address(callback: (err: Error, addr: { ip: string, ipv6: string, mac: string }) => void): void
-function address(interfaceName: string, callback: (err: Error, addr: { ip: string, ipv6: string, mac: string }) => void): void
+declare function address(callback: (err: Error, addr: { ip: string, ipv6: string, mac: string }) => void): void
+declare function address(interfaceName: string, callback: (err: Error, addr: { ip: string, ipv6: string, mac: string }) => void): void
 
 export = address
