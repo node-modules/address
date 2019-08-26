621f8f2c3b9ba044cdd7d8f53e201482e6cf5a5c
wanghx <whx89768@alibaba-inc.com>
2019-08-26 15:25:31 +0800

    f

--- test/ts.test.js
+++ test/ts.test.js
@@ -2,11 +2,13 @@ const runscript = require('runscript');
 const path = require('path');
 const assert = require('assert');
 
-describe('test/ts.test.js', () => {
-  it('should works with ts without error', async () => {
+describe('test/ts.test.js', function() {
+  it('should works with ts without error', function (done) {
     const cwd = path.resolve(__dirname, './fixtures/ts');
     const tsconfigPath = path.resolve(cwd, 'tsconfig.json');
     const testFile = path.resolve(cwd, 'test');
-    await runscript(`tsc -p ${tsconfigPath} && node ${testFile}`, { stdio: 'inherit' });
+    runscript(`tsc -p ${tsconfigPath} && node ${testFile}`, { stdio: 'inherit' })
+      .then(() => done())
+      .catch(done);
   });
 });
