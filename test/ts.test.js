f21698197e033144d56b7a81d51f2be27b55e50e
fengmk2 <fengmk2@gmail.com>
2023-09-22 21:17:18 +0800

    test: enable test on 1.x

--- test/ts.test.js
+++ test/ts.test.js
@@ -5,8 +5,7 @@ describe('test/ts.test.js', () => {
   it('should works with ts without error', done => {
     const cwd = path.resolve(__dirname, './fixtures/ts');
     const tsconfigPath = path.resolve(cwd, 'tsconfig.json');
-    const testFile = path.resolve(cwd, 'test');
-    runscript(`tsc -p ${tsconfigPath} && node ${testFile}`, { stdio: 'inherit' })
+    runscript(`tsc -p ${tsconfigPath}`, { stdio: 'inherit' })
       .then(() => done())
       .catch(done);
   });
